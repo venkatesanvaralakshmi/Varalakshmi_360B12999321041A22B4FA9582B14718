921793b417cb16e6761cc85c5ee5bdccf237c75e
G.Lokeshpriya <147473744+venkatesanvaralakshmi@users.noreply.github.com>
2023-11-01 15:17:49 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
-# 1.1 Implement a recursive function to calculate the factorial of a given number.
-
-"""
-1! = 1 x 1
-2! = 2 x 1! --->2 x 1
-3! = 3 x 2! --->3 x 2 x 1
-.
-.
-10! = 10 x 9! --->10 x 9 x 8 x... x 1
-
-Formula - n x (n-1)!
-"""
-
-
-def fact_rec(n):
-  if n==0 or n==1:
-    return 1
-  else:
-    return n*fact_rec(n-1)
-
-number = int(input("Enter a value:"))
-res = fact_rec(number)
-
-print("The factorial of {} is {}.".format(number,res))
+def recur_factorial(n):  
+   if n == 1:  
+       return n  
+   else:  
+       return n*recur_factorial(n-1)  
+# take input from the user  
+num = int(input("Enter a number: "))  
+# check is the number is negative  
+if num < 0:  
+   print("Sorry, factorial does not exist for negative numbers")  
+elif num == 0:  
+   print("The factorial of 0 is 1")  
+else:  
+   print("The factorial of",num,"is",recur_factorial(num))
